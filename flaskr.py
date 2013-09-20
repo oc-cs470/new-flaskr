b43990f72c4e90b6a6796634f71654a3b8e40a44
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2013-09-20 10:26:50 -0400

    Add users table and simple user display page.

--- flaskr.py
+++ flaskr.py
@@ -69,13 +69,21 @@ def show_entries():
     return render_template('show_entries.html', entries=entries)
 
 
+@app.route('/users')
+def show_users():
+    db = get_db()
+    cur = db.execute('SELECT * FROM users ORDER BY last_name ASC, first_name ASC')
+    users = cur.fetchall()
+    return render_template('show_users.html', users=users)
+
+
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
     db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+               [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
--- schema.sql
+++ schema.sql
@@ -1,6 +1,19 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  text text not null
+CREATE TABLE IF NOT EXISTS entries (
+  id integer PRIMARY KEY autoincrement,
+  title TEXT NOT NULL,
+  text TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY,
+    password TEXT,
+    email TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
+    role integer,
+    dob date
+);
+
+INSERT OR IGNORE INTO users (username, password, email, first_name, last_name)
+    VALUES ("admin", "password", "email@site.com", "Site", "Administrator");
+
